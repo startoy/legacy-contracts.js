cfb9e8017a8ada2489dba6d865baf282eeb936ca
David Braun <David@Monax.io>
2016-12-23 19:46:52 -0500

    Publish documentation to new Monax web site.
    Fixes #57.

--- JSDoc/publish.sh
+++ JSDoc/publish.sh
@@ -1,32 +1,27 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# Publish documentation to the Eris website.
+# Publish documentation to the Monax website.
 
-name=$(basename $PWD)
-docs_site=docs.erisindustries.com
+name=contracts.js
+repository=monax.io
 repo=$PWD
-release_min=$(cat package.json | jq --raw-output .version)
+version=$(jq --raw-output .version package.json)
 
 # Build
-
 npm run doc
-
 cd $HOME
-git clone git@github.com:eris-ltd/$docs_site.git
-cd $docs_site/documentation
+git clone git@github.com:eris-ltd/$repository.git
+cd $repository/content/docs/documentation
 mkdir --parents $name
 cd $name
-mv $repo/doc $release_min
-ln --symbolic $release_min latest
+mv $repo/doc $version
+ln --symbolic $version latest
 
 # Commit and push if there are changes.
-
-if [ -z "$(git status --porcelain)" ]; then
-  echo "All Good!"
-else
+if [ -n "$(git status --porcelain)" ]; then
   git config --global user.email "billings@erisindustries.com"
   git config --global user.name "Billings the Bot"
   git add -A :/ &&
   git commit --message "$name build number $CIRCLE_BUILD_NUM doc generation" &&
-  git push origin master
+  git push origin staging
 fi
