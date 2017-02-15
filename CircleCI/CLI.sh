583d47bddfa63023d837fbd88433a6a4b0ffd088
David Braun <David@Monax.io>
2017-02-15 14:09:29 -0500

    Add Vagrant configuration for testing.

--- CircleCI/CLI.sh
+++ CircleCI/CLI.sh
@@ -8,7 +8,7 @@ if [ $CIRCLE_BRANCH = 'master' ]; then
   sudo add-apt-repository https://apt.monax.io
   curl -L https://apt.monax.io/APT-GPG-KEY | sudo apt-key add -
   sudo apt-get --quiet update
-  sudo apt-get install --assume-yes --quiet eris
+  sudo apt-get install --quiet --yes eris
 else
   go get github.com/eris-ltd/eris/cmd/eris
   cd $HOME/.go_workspace/src/github.com/eris-ltd/eris/cmd/eris
