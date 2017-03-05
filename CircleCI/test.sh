e6b0ba7dc835ef880737169226835fce78fe13b8
David Braun <David@Monax.io>
2017-03-05 13:59:07 -0500

    CircleCI: Make blockchain only when testing ‘master’ branch.

--- CircleCI/test.sh
+++ CircleCI/test.sh
@@ -5,8 +5,25 @@ set -o xtrace
 # Test against mock Eris DB.
 npm test
 
-if [ $CIRCLE_BRANCH = 'master' ]; then
-  # Test Eris DB against our expectations of it.
+if [ "$CIRCLE_BRANCH" = "master" ]; then
+  # Install Docker Machine because CircleCI's Docker support is hobbled.
+  curl -L https://github.com/docker/machine/releases/download/$DOCKER_MACH\
+  INE_VERSION/docker-machine-`uname -s`-`uname -m` > $HOME/bin/docker-machi\
+  ne && chmod +x $HOME/bin/docker-machine
+
+  docker-machine create --driver digitalocean default
+  eval $(docker-machine env default)
+
+  # Install Eris CLI.
+  sudo add-apt-repository https://apt.monax.io
+  curl -L https://apt.monax.io/APT-GPG-KEY | sudo apt-key add -
+  sudo apt-get --quiet update
+  sudo apt-get install --assume-yes --quiet eris
+  eris init
+
+  # Make a blockchain for testing.
   npm run make
-  eval $(docker-machine env default); TEST=contract npm test
+
+  # Test Eris DB against our expectations of it.
+  TEST=server npm test
 fi
